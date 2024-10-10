2404119b0329dd03ef2c531cf5dc7c4bf6571ee2
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-10-10 18:12:57 +0400

    feat: create access control for user

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,3 +1,5 @@
+import { admin_only_field } from '@/access/admin_only'
+import { Admin_or_Self } from '@/access/admin_or_self'
 import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
@@ -6,8 +8,15 @@ export const Users: CollectionConfig = {
     useAsTitle: 'email',
   },
   auth: true,
+  access: {
+    update: Admin_or_Self,
+    delete: Admin_or_Self,
+  },
   fields: [
     {
+      access: {
+        update: admin_only_field,
+      },
       name: 'role',
       type: 'select',
       options: ['user', 'stadium_owner', 'admin'],
