2067ac915d0f4f126787dedba187bed27c235782
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-10-12 01:35:46 +0400

    feat: add access control function for admin or captain

--- src/access/captain.ts
+++ src/access/captain.ts
@@ -1,8 +1,22 @@
 import { Access } from "payload";
-export const CaptainOnly: Access = async ({ req: { user } }) => {
+export const CaptainOnly: Access = ({ req: { user } }) => {
   if (!user) {
     return false;
   } else {
+    return {
+      captain: {
+        equals: user?.id
+      }
+    }
+  }
+}
+export const AdminOrCaptain: Access = ({ req: { user } }) => {
+  if (!user) {
+    return false;
+  } else {
+    if (user.role === 'admin') {
+      return true;
+    }
     return {
       captain: {
         equals: user?.id
