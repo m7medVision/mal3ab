607f92c3da2fc876baf07378dd8efce74b0d1b6d
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-10-18 20:40:45 +0400

    feat: write home page in temp. vertion

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,29 @@
-export default function Page() {
+import { Button, buttonVariants } from '@/components/ui/button';
+import WordRotate from '@/components/ui/word-rotate';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Home() {
   return (
-    <div>
-      <h1>Page</h1>
+    <div className="h-screen bg-background flex items-center">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl font-bold mb-4">
+          <span>Book a Stadium </span>
+          <WordRotate words={["Today", "Now", "Right Now"]} />
+        </h1>
+        <p className="text-lg mb-6">
+          Connecting stadium owners and players for seamless booking and easier splits.
+        </p>
+        <div className="flex justify-center gap-2">
+          <Button>
+            List Your Stadium
+          </Button>
+          <Link href="/stadiums" className={buttonVariants({
+          })}>
+            Find a Stadium
+          </Link>
+        </div>
+      </div>
     </div>
   );
-}
+};
