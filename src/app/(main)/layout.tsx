47b6cbc56da61c9f8580ab5f6088cea01abbb850
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-10-18 16:52:05 +0400

    feat: add tailwind css

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Mal3ab - Home',
+  description: 'Mal3ab is a platform to make booking stadiums easier',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
