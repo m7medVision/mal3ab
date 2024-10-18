0787eed8f631c3d92574bd7505f8e6c8d9c636b4
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-10-18 20:39:33 +0400

    chore: allow fake image api temp.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,13 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
