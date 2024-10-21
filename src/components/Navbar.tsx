c237aae275201f88edcd5bc418b3fc7ecaf27f75
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-10-21 21:13:52 +0400

    feat: write navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-around py-4">
+      <ul className="flex content-center items-end">
+        <li className="mr-6">
+          <a className="text-3xl font-bold" href="#">Mal3ab</a>
+        </li>
+      </ul>
+      <Button>Login</Button>
+    </nav>
+  )
+}
+
