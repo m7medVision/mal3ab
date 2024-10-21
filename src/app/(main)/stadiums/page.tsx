866a849a324e3cc77b6302c9e23d35bc9e2fea12
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-10-21 21:16:51 +0400

    feat: write stadiums page

--- src/app/(main)/stadiums/page.tsx
+++ src/app/(main)/stadiums/page.tsx
@@ -2,13 +2,14 @@ import { StadiumCardComponent } from "@/components/stadium-card";
 import { faker } from "@faker-js/faker"
 export default function Page() {
   return (
-    <div className="flex flex-col gap-4 items-center justify-center">
-      <h1 className="text-4xl font-bold pt-10">Book a Stadium</h1>
+    <div className="mx-auto w-5/6">
+      <h1 className="text-4xl font-bold pt-10 pb-2">Book a Stadium</h1>
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {
           Array.from({ length: 18 }).map((_, index) => (
             <StadiumCardComponent
               key={index}
+              id={index}
               name={faker.company.name()}
               location={faker.address.streetAddress()}
               price={Number(faker.commerce.price())}
