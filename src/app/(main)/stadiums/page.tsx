8e72383d5c201cc1fbd54980108af435b3b94e23
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-10-25 21:11:58 +0400

    chore: connect page with database

--- src/app/(main)/stadiums/page.tsx
+++ src/app/(main)/stadiums/page.tsx
@@ -1,27 +1,30 @@
 import { StadiumCardComponent } from "@/components/stadium-card";
-import { faker } from "@faker-js/faker"
 import { getPayloadHMR } from "@payloadcms/next/utilities"
 import config from "@payload-config"
 export default async function Page() {
   const payload = await getPayloadHMR({ config })
-  console.log(await payload.find({
-    collection: "stadiums",
-  }))
+  const stadiums = await payload.find({
+    collection: 'stadiums',
+    limit: 20,
+  })
   return (
     <div className="mx-auto w-5/6">
       <h1 className="text-4xl font-bold pt-10 pb-2">Book a Stadium</h1>
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {
-          Array.from({ length: 18 }).map((_, index) => (
-            <StadiumCardComponent
-              key={index}
-              id={index}
-              name={faker.company.name()}
-              location={faker.address.streetAddress()}
-              price={Number(faker.commerce.price())}
-              images={["https://picsum.photos/1000/1000?random=1", "https://picsum.photos/1000/1000?random=2", "https://picsum.photos/1000/1000?random=3"]}
-            />
-          ))
+          stadiums.docs.map((stadium) => {
+            return (
+              <StadiumCardComponent
+                key={stadium.id}
+                id={stadium.id}
+                name={stadium.name}
+                location={stadium.location.address}
+                images={[]}
+                price={stadium.hourlyRate}
+              />
+            )
+          }
+          )
         }
       </div>
     </div>
