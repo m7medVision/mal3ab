b14956dbab5c715df982730bd9beab5d5e40aeb9
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-11-15 17:15:59 +0400

    fix: update Navbar link to point to home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,16 @@
-import { Button } from "./ui/button";
+import { Button } from './ui/button'
 
 export default function Navbar() {
   return (
     <nav className="flex justify-around py-4">
       <ul className="flex content-center items-end">
         <li className="mr-6">
-          <a className="text-3xl font-bold" href="#">Mal3ab</a>
+          <a className="text-3xl font-bold" href="/">
+            Mal3ab
+          </a>
         </li>
       </ul>
       <Button>Login</Button>
     </nav>
   )
 }
-
